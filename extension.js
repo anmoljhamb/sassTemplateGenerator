8b8fcf5092bd092171c5d5a5324b3f3ec10ce94a
Anmol Jhamb <talktoanmol@outlook.com>
2023-01-13 14:16:43 +0530

    changed title of the command

--- extension.js
+++ extension.js
@@ -1,36 +1,28 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const vscode = require("vscode");
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+    console.log(
+        'Congratulations, your extension "sassTemplateGenerator" is now active!'
+    );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "sassTemplateGenerator" is now active!');
+    let disposable = vscode.commands.registerCommand(
+        "sassTemplateGenerator.generateSass",
+        function () {
+            vscode.window.showInformationMessage(
+                "Hello World from Sass Template Generator!"
+            );
+        }
+    );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('sassTemplateGenerator.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Sass Template Generator!');
-	});
-
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+    activate,
+    deactivate,
+};
