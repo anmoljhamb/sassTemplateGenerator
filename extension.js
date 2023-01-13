760048c947912a59ddb5e68c16fa20f0487543e1
Anmol Jhamb <talktoanmol@outlook.com>
2023-01-13 14:38:43 +0530

    added todos, and changed module name from camelCase to snake_case

--- extension.js
+++ extension.js
@@ -4,8 +4,16 @@ const vscode = require("vscode");
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+    /**
+     * todo get the selected text from a particular document.
+     * todo convert the selected html/jsx to a boiler template in SASS.
+     * todo create a new file with the same name as the input document, except with extension as scss.
+     * todo add an import line in the current document file.
+     * todo open the newly created scss file.
+     */
+
     console.log(
-        'Congratulations, your extension "sassTemplateGenerator" is now active!'
+        'Congratulations, your extension "sass_template_generator" is now active!'
     );
 
     let disposable = vscode.commands.registerCommand(
