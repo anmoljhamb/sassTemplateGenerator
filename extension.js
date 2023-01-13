7caad04c9c8ccebf2e82977cba04963f54a447a6
Anmol Jhamb <talktoanmol@outlook.com>
2023-01-13 14:45:37 +0530

    todo complete - create a new file with the same filename except the file extension in the same folder

--- extension.js
+++ extension.js
@@ -1,15 +1,21 @@
 const vscode = require("vscode");
+const fs = require("fs");
+
+const replaceFileExtension = (fileName, extension = ".scss") => {
+    const exp = /\.([A-Za-z0-9]+)$/;
+    return fileName.replace(exp, extension);
+};
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
     /**
-     // // * todo get the selected text from a particular document.
-     * todo convert the selected html/jsx to a boiler template in SASS.
-     * todo create a new file with the same name as the input document, except with extension as scss.
-     * todo add an import line in the current document file.
-     * todo open the newly created scss file.
+     // // todo get the selected text from a particular document.
+     // todo convert the selected html/jsx to a boiler template in SASS.
+     // // todo create a new file with the same name as the input document, except with extension as scss.
+     // todo add an import line in the current document file.
+     // todo open the newly created scss file.
      */
 
     console.log(
@@ -21,8 +27,14 @@ function activate(context) {
         function () {
             const editor = vscode.window.activeTextEditor;
             const text = editor.document.getText(editor.selection);
+            const newFilePath = replaceFileExtension(editor.document.fileName);
+
+            fs.writeFileSync(newFilePath, text, {
+                encoding: "utf-8",
+            });
+
             vscode.window.showInformationMessage(
-                `Selected text from the current document is: ${text}`
+                `SCSS Boiler Template Generated at the path: ${newFilePath}`
             );
         }
     );
